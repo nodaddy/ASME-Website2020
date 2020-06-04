fa964bbc1787825da6d20a3cb290af834455f6b2
Darkshredder <srivastavayash58@gmail.com>
2020-06-04 15:10:52 +0530

    Team page components made just assests and all details need to be implemented

--- asme/src/App.js
+++ asme/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+import Socialrec from './components/socialReac'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Team from './components/Team'
 function App() {
   return (
-    <div className="App">
+    <div>
+    <Socialrec />
     <Navbar />
     <Router>
     <Route
