9a1bee4861e2e5d655216fbd2181321071ad6510
Yash <srivastavayash58@gmail.com>
2020-07-27 21:44:01 +0530

    fixes

--- asme/src/App.js
+++ asme/src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
-import './App.css';
-import Socialrec from './components/socialReac'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Team from './components/Team'
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Socialrec from "./components/socialReac";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Team from "./components/Team";
+import ContactUs from "./components/ContactUs";
+import Resource from "./components/Resources";
 function App() {
   return (
     <div>
-    <Socialrec />
-    <Navbar />
-    <Router>
-    <Route
-          exact
-          path={`/`}
-          component={ Home }
-        />
-    <Route
-          exact
-          path={`/team`}
-          component={ Team }
-        />
-    </Router>  
+      <Socialrec />
+      <Navbar />
+      <Router>
+        <Route exact path={`/`} component={Home} />
+        <Route exact path={`/team`} component={Team} />
+        <Route exact path={`/contactus`} component={ContactUs} />{" "}
+        <Route exact path={`/resources`} component={Resource} />
+      </Router>
     </div>
   );
 }
